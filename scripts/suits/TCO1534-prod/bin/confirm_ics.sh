09eb97c37e2415c570ae27828d1040cf7d8bebb8
gefsopera <gefsopera@tropmet.res.in>
2022-12-10 17:12:54 +0530

    Updated flow

--- scripts/suits/TCO1534-prod/bin/confirm_ics.sh
+++ scripts/suits/TCO1534-prod/bin/confirm_ics.sh
@@ -4,23 +4,34 @@
 
 dstamp=${CYLC_TASK_CYCLE_POINT:0:8}
 idir="$IC_DIR/gfs.${dstamp}"
+rdir="/home/gfsprod/data/gdasv14/gdas/prod/gdas.${dstamp}"
+server="iitmgefs@10.119.105.5"
 
-files="gfs.t00z.sfcanl.nemsio gfs.t00z.nstanl.nemsio gfs.t00z.atmanl.nemsio"
+# files="gfs.t00z.sfcanl.nemsio gfs.t00z.nstanl.nemsio gfs.t00z.atmanl.nemsio"
 
-for f in $files; do
-    fpath=${idir}/${f}
-    echo "Checking for $fpath $CYLC_TASK_CYCLE_POINT"
-    if [[ ! -f "$fpath" ]]; then
-        echo "File $fpath doesn't exist"
-        exit 1
-    fi
+filesStr="sfcanl nstanl atmanl"
 
-    date1=$(stat -c %y "$fpath")
+# ssh iitmgefs@10.119.105.5 stat -c %y ${rdir}/gdas1.t00z.atmanl.nemsio
+mkdir -p $idir
+
+for fs in $filesStr; do
+
+    rfpath=${rdir}/gdas1.t00z.${fs}.nemsio
+    echo "Checking for $server $rfpath"
+    date1=$(ssh $server stat -c %y ${rfpath}) || (echo $date1; exit 1)
     sleep 5
-    date2=$(stat -c %y "$fpath")
+    date2=$(ssh $server stat -c %y ${rfpath}) || (echo $date2; exit 1)
     if [[ "$date1" != "$date2" ]]; then
-        echo "File $fpath is still getting updated"
+        echo "File $rfpath is still getting updated..."
         exit 1
     fi
+
 done
+
+for fs in $filesStr; do
+    rfpath=${rdir}/gdas1.t00z.${fs}.nemsio
+    ifpath=${idir}/gfs.t00z.${fs}.nemsio
+    scp ${server}:${rfpath} ${ifpath} || (echo "Error while coping $rfpath"; exit 1) || exit 1
+done
+
 exit 0
